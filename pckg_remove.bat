83fe1eeac2c57d39b86b0aa2957f822af544d434
Ingo Breßler <dev@ingobressler.net>
2017-10-04 18:25:08 +0200

    pckg_remove: fixing unprovisioning of packages

--- pckg_remove.bat
+++ pckg_remove.bat
@@ -21,6 +21,7 @@ fltmc >nul 2>&1 && (
 	rem exit /b
 )
 
+set "DBG="
 rem extracts usernames from between brackets [], mind the escape char ^
 set userpattern=\"\[^([^^^^\]]+^)\]\"
 set tmpfile=%TEMP%\_pckg_remove_tmp.txt
@@ -30,28 +31,30 @@ set users=
 setlocal enabledelayedexpansion
 for %%a in (%*) do (
 	rem for each provided package name search key
-	echo Uninstall installed package '%%a' first
+	echo Uninstalling packages matching '%%a'
 	rem get the full package name first
-	set basecmd=get-appxpackage -AllUsers ^| where-object {$_.name -like \"*%%a*\"}
+	set basecmd=get-appxpackage -AllUsers ^| sort packagefullname ^| where-object {$_.name -like \"*%%a*\"}
 	set pfncmd=powershell -Command "& {!basecmd! | select-string -inputobject {$_.packagefullname} -pattern ".*" -all; }"
-	echo pfncmd: !pfncmd!
-	!pfncmd!
+	if defined DBG (
+		echo pfncmd: !pfncmd!
+		!pfncmd! )
 	for /f "tokens=*" %%i in ('!pfncmd!') do (
 		rem for each package matching the given search key
 		set pfn=%%i
-		echo pfn: '!pfn!'
+		if defined DBG echo pfn: '!pfn!'
 		set usercmd=powershell -Command "& {get-appxpackage -AllUsers ^| where-object {$_.packagefullname -like \"!pfn!\"} ^| select-string -inputobject {$_.packageuserinformation} -pattern %userpattern% -all ^| %% { $_.Matches } ^| %% { $_.captures.groups[1].Value } }"
-		rem  echo usercmd: !usercmd!
-		rem  !usercmd!
-		rem this does not work for some reason (pattern?)
+		if defined DBG (
+			echo usercmd: !usercmd!
+			!usercmd! )
+		rem FOR does not work for some reason (pattern?)
 		rem for /f "tokens=*" %%u in ('!usercmd!') do echo un: '%%u'
 		rem using a temorary file to get the user names
 		!usercmd! > %tmpfile%
-		echo start:
+		if defined DBG echo  start:
 		for /f %%u in (%tmpfile%) do (
 			echo Stage package !pfn! to be removed for %%u
 			set scriptabs=%appdata%\..\..\..\%%u\%autostartscript%
-			echo as: '!scriptabs!'
+			if defined DBG echo autostartscript: '!scriptabs!'
 			rem add each user to the list once
 			set "TRUE="
 			rem username is not in the list yet
@@ -67,27 +70,29 @@ for %%a in (%*) do (
 			echo echo Removing '!pfn!'>> "!scriptabs!"
 			echo pause>> "!scriptabs!"
 			set rmcmd=powershell -Command "& {remove-appxpackage !pfn!}"
-			echo rem !rmcmd! >> "!scriptabs!"
+			echo !rmcmd! >> "!scriptabs!"
 		)
 	)
-	echo done.
-	rem echo Remove '%%a' for new users (provisioned)
-	rem powershell -Command "& {get-appxprovisionedpackage -online | where-object {$_.packagename -like \"*%%a*\"} | select packagename | remove-appxprovisionedpackage -online;}"
+	echo Unprovisioning packages matching '%%a' for new users.
+	set unprovcmd=powershell -Command "& {get-appxprovisionedpackage -online | where-object {$_.packagename -like \"*%%a*\"} | remove-appxprovisionedpackage -online }"
+	if defined DBG echo unprovcmd: !unprovcmd!
+	!unprovcmd!
 )
 :end
+
 if exist %tmpfile% del %tmpfile%
-echo users found: !users!
+if defined DBG echo users processed: !users!
 for %%u in (!users!) do (
-	echo user: %%u
+	if defined DBG echo autostartscript for '%%u':
 	set scriptabs=%appdata%\..\..\..\%%u\%autostartscript%
-	echo as: '!scriptabs!'
+	if defined DBG echo '!scriptabs!'
 	if exist "!scriptabs!" (
 		echo set scriptpath=%%~dpf0>> "!scriptabs!"
-		echo echo Finally removing '%%scriptpath%%'>> "!scriptabs!"
-		echo exit /b>> "!scriptabs!"
+		echo echo Finally cleaning up>> "!scriptabs!"
+		echo echo '%%scriptpath%%'>> "!scriptabs!"
+		rem echo exit /b>> "!scriptabs!"
 		echo pause>> "!scriptabs!"
-		echo del %%scriptpath%% >> "!scriptabs!"
-		echo del "!scriptabs!" >> "!scriptabs!"
+		echo del "%%scriptpath%%" >> "!scriptabs!"
 	)
 )
 
