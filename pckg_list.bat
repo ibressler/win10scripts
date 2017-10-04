9ffd7fcd6ae09f85624746633d3608d1e387db2a
Ingo Breßler <dev@ingobressler.net>
2017-10-04 14:19:40 +0200

    script for listing installed/prov. Win. apps

---
+++ pckg_list.bat
@@ -0,0 +1,27 @@
+@echo off
+rem pckg_list.bat
+rem Lists installed or provisioned Windows (modern app) packages.
+rem Tested with Win 10 Pro 64bit.
+rem by Ingo Breßler in 2017 (dev at ingobressler.net)
+cls
+
+rem Check for admin permissions
+fltmc >nul 2>&1 || (
+	echo Please run with admin permissions!
+	exit /b
+)
+
+if "%*"=="" (
+	echo packages:
+	powershell -Command "& {get-appxpackage -AllUsers | select name;}"
+
+	echo packages provisioned:
+	powershell -Command "& {get-appxprovisionedpackage -online | select displayname;}"
+) else (
+	for %%p in (%*) do (
+		echo Searching for %%p:
+		powershell -Command "& {get-appxpackage -AllUsers | where-object {$_.name -like \"*%%p*\"}; }"
+	)
+)
+
+pause
